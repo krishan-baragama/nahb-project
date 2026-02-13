73f78702dea88715e59f929e16412a0694e946d5
krishan-baragama <krishan.baragama-acharige@epita.fr>
2026-02-13 16:56:39 +0100

    fix: update edit_story & statistics files

--- django-app/djangoProject/gameplayApp/templatetags/dict_filters.py
+++ django-app/djangoProject/gameplayApp/templatetags/dict_filters.py
@@ -6,3 +6,10 @@ register = template.Library()
 def lookup(dictionary, key):
     """Get dictionary value by key"""
     return dictionary.get(key)
+
+@register.filter
+def items(dictionary):
+    """Get dictionary items for iteration"""
+    if dictionary:
+        return dictionary.items()
+    return []
